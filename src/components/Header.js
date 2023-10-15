91d55b7e317f1590448aaceacbb96bc41935bc78
Charanaraj N <goudacharanraj760@gmail.com>
2023-10-15 11:56:17 +0530

    toggle menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
   return (
     <div className="grid grid-flow-col p-5 shadow-lg items-center">
       <div className="flex col-span-1">
         <img
-          className="h-6"
+          onClick={() => toggleMenuHandler()}
+          className="h-6 cursor-pointer"
           alt="menu"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
         />
