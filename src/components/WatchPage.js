ae36ce5452960442780c4c8c1ffb83bf6f197d86
Charanaraj N <goudacharanraj760@gmail.com>
2023-10-15 13:42:49 +0530

    chore: react router && watch page

---
+++ src/components/WatchPage.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
+
+const WatchPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
+  return (
+    <div className="mt-4 mx-16">
+      <iframe
+        width="1000"
+        height="500"
+        src={`https://www.youtube.com/embed/${searchParams.get(
+          "v"
+        )}?&autoplay=1&mute=1`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullscreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -3,15 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const appSlice = createSlice({
   name: "app",
   initialState: {
-    isMenuOpen: false,
+    isMenuOpen: true,
   },
   reducers: {
     toggleMenu: (state) => {
       state.isMenuOpen = !state.isMenuOpen;
     },
+    closeMenu: (state) => {
+      state.isMenuOpen = false;
+    },
   },
 });
 
-export const { toggleMenu } = appSlice.actions;
+export const { toggleMenu, closeMenu } = appSlice.actions;
 
 export default appSlice.reducer;
