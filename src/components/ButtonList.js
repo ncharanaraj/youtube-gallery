6e1d66216380aab204c5f16f79ff8e9b2b5c21f4
Charanaraj N <goudacharanraj760@gmail.com>
2023-10-15 12:13:19 +0530

    button && buttonList

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Button from "./Button";
+
+const list = [
+  "All",
+  "Gaming",
+  "Songs",
+  "Live",
+  "Love",
+  "Cooking",
+  "News",
+  "Cinema",
+  "Editing",
+  "UI/UX",
+  "Git/GitHub",
+  "Software",
+];
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex">
+      {list.map((catName) => (
+        <Button key={catName} name={catName} />
+      ))}
+    </div>
+  );
 };
 
 export default ButtonList;
